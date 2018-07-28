aab8310c986c78cd2f1af7a2c5a9abc9bb405baa
Sean Obringer <SeanObringer90@gmail.com>
2018-07-28 16:32:48 -0700

    added branding font

--- src/components/events.js
+++ src/components/events.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-
+import '../App.css'
 class Events extends Component {
 
   render() {
     return (<div>
-      <h3>EVENTS</h3>
-
-      <h4>Shows 2018-2019</h4>
-      <ul>
-        <li>Bellevue 6th Street Fair July 27 - 29</li>
-        <li> Anacortes Art Festival  Aug 3 - 5</li>
-        <li> Bainbridge Island Studio Tour  August 10 - 12</li>
-        <li> Bainbridge Island Studio Tour Dec 1 - 3</li>
-        <li> Northwest Flower and Garden Show Feb 20 - 24, 2019</li>
+      <h3> EVENTS</h3>
+      <h4 style={{paddingTop:'2rem'}}>Shows 2018-2019</h4>
+      <ul className='eventLink'>
+        <li > <a href='https://www.bellevuedowntown.com/events/6th-street-fair'>Bellevue 6th Street Fair July 27 - 29</a></li>
+        <li> <a href='http://anacortesartsfestival.com/'>Anacortes Art Festival  Aug 3 - 5</a></li>
+        <li> <a href='http://www.bistudiotour.com/'>Bainbridge Island Studio Tour  August 10 - 12 </a></li>
+        <li> <a href='http://www.bistudiotour.com/'>Bainbridge Island Studio Tour Dec 1 - 3</a></li>
+        <li><a href='https://www.gardenshow.com/'>Northwest Flower and Garden Show Feb 20 - 24, 2019</a> </li>
       </ul>
     </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -10,9 +10,8 @@ class Home extends Component {
 
         <CarouselComponent />
         <div className='homeCopy'>
-          <p>I’m intrigued by the alchemy of felt making.</p>
           <p>
-            For the PT Wearable Art show (Port Townsend), I used the extremes of wool’s capability. I’ve tried my hand with the delicate application of nuno felting to create a wedding dress. The Animals in the Landscape Coat was inspired by a Franz Marc painting. My ‘painting’ wrapped around the long coat and it’s circular draped tail added drama to its movement. The Horned Mask is a densely felted sculpture.  The possibilities to create with wool are endless.
+          I’m intrigued by the alchemy of felt making. For the PT Wearable Art show (Port Townsend), I used the extremes of wool’s capability. I’ve tried my hand with the delicate application of nuno felting to create a wedding dress. The Animals in the Landscape Coat was inspired by a Franz Marc painting. My ‘painting’ wrapped around the long coat and it’s circular draped tail added drama to its movement. The Horned Mask is a densely felted sculpture.  The possibilities to create with wool are endless.
           </p>
           <p>Nuno Felting is achieved through the stacking & layering of merino wool fibers onto hand dyed silk, carefully varying their lengths & alignments in a fashion that allows for controlled shrinkage. The resulting felt maintains just the right amount of movement & drape for its function as good design & clothing. </p>
           <p> - Donna</p>
